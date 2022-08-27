bfba18533820d8d92e59ce44cf486daff357f0d7
drako9159 <chochito9159@gmail.com>
2022-08-27 13:41:15 -0500

    change configs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://drako9159.github.io/webPage_rickAndMorty/",
+ // base: "https://drako9159.github.io/webPage_rickAndMorty/",
   plugins: [react()]
 })
