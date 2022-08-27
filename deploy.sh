9b517cf7b24b6c349c8ff64fc69b697da05cee31
drako9159 <chochito9159@gmail.com>
2022-08-27 13:30:39 -0500

    1.0

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,6 @@ git init
 git checkout -b main
 git add -A
 git commit -m 'deploy'
-
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
